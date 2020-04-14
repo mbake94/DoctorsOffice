29c7e1f7793862d371c57c9441800634406e28e5
mbake94 <mbake94@gmail.com>
2020-04-14 08:54:15 -0700

    Doctors Office assignment

---
+++ src/DoctorsOffice.java
@@ -0,0 +1,17 @@
+public class DoctorsOffice {
+    public static void main(String[] args) {
+
+        String patientName= "Stacey Manson";
+        Boolean hasInsurance= false;
+        double temp= 102.2;
+        double numVisits= 5.02;
+
+        System.out.println("Patient Details:");
+        System.out.println("Name: "+ patientName);
+        System.out.println("Patient has insurance: " + hasInsurance);
+        System.out.println("Body temperature: " + temp);
+
+
+    }
+
+}
