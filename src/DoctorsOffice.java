fcb6741ee31c99fced417f6eb1eced603bc65882
mbake94 <mbake94@gmail.com>
2020-04-14 08:57:51 -0700

    whatever-commit-message

--- src/DoctorsOffice.java
+++ src/DoctorsOffice.java
@@ -4,12 +4,13 @@ public class DoctorsOffice {
         String patientName= "Stacey Manson";
         Boolean hasInsurance= false;
         double temp= 102.2;
-        double numVisits= 5.02;
+        int numVisits= 5;
 
         System.out.println("Patient Details:");
         System.out.println("Name: "+ patientName);
         System.out.println("Patient has insurance: " + hasInsurance);
         System.out.println("Body temperature: " + temp);
+        System.out.println("Number of visits: " + numVisits);
 
 
     }
